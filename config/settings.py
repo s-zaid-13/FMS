b2f6c9cac7d8c988195743d53288580ab7efc876
Samama Zaid <s.zaid43111@gmail.com>
2026-03-21 01:20:10 +0500

    Updated ALLOWED_HOSTS for Vercel deployment

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
+print(ALLOWED_HOSTS)
 
 # Application definition
 
